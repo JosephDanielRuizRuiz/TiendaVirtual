aa241652a2ec469a1512ec7cc35bf236925d0f69
RGH-LS24-19 <RGH-LS24-19@UMD.LOCAL.EDU>
2024-05-06 08:08:35 -0500

    agregar informacion

---
+++ src/app/tabla/SobreNosotros.php
@@ -0,0 +1,88 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/SobreNosotros.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend&display=swap" rel="stylesheet">
+ 
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div id="cabeza" class="encabezado">
+            <h1 class="Logo">Heftziba Cabina Estetica</h1>
+                <nav>
+                    <a href="./index.html" class="active">Inicio</a>
+                    <a href="./SobreNosotros.html">Sobre nosotros</a>
+                    <a href="./Servicios.php">Servicios</a>
+                    <a href="./indexcrud.html">Usuarios Registrados</a>
+                    <a href="./Carrito.html">Mi Carrito</a>
+                    <a href="./lnicioSesion.html">Inicio Sesion</a>
+                </nav>            
+        </div>
+    </header>
+
+    <main>
+        <div class="seccion-pagina">
+            <div class="contenedor">
+                <h1 class="Subtitulo">Heftziba, tu Bienestar en nuestras <span>Manos</span></h1>
+                <p class="descripcion">Nos encargamos de ofrecer los mejores servicios para mejorar tu salud, aspecto, y vida</p>
+            </div>
+        </div>
+    </main>
+
+    <div class="contenedor-imagenes">
+        <p><h1>Homeopatia</h1>
+        <br> 
+        Es una práctica que consiste en administrar a alguien, en dosis mínimas, las mismas sustancias que, en mayores cantidades, producirían supuestamente en la persona sanar síntomas iguales o parecidos a los que se trata de combatir
+        <br>
+        <br>
+        Los remedios homeopáticos se preparan mediante sucesivas diluciones en agua o alcohol, considerando que, cuanto más se diluya la sustancia, más potente será el medicamento. Los productos que se utilizan pueden ser de origen animal, vegetal o mineral.
+        </p>
+        <p>
+            <h1>Masaje Relajante</h1>
+            <br>
+            El masaje relajante consiste en la realización de maniobras superficiales en las que la intensidad de la presión es suave y el ritmo lento y reiterativo, de manera que al recibir un contacto repetido y constante, se pierde la sensación de dolor y los músculos se relajan.
+            <br>
+            <br>
+            El objetivo principal de este masaje es la secreción de endorfinas que se produce durante su aplicación, esta hormona también conocida como la hormona de la felicidad, ayuda a aliviar tensiones musculares, mejorar la circulación y conseguir disminución del estrés y la ansiedad.
+            
+            
+        </p>
+        <p>
+            <h1>Plasma Rico En Plaquetas</h1><br>El PRP se define como una fracción de plasma obtenido de sangre autóloga que tiene una concentración de plaquetas superior a la del plasma en condiciones basales. El PRP contiene no solo un alto nivel de plaquetas, sino también de los factores de crecimiento que son secretados activamente por las plaquetas.
+            <br>
+            <br>
+           La aplicación del plasma rico en plaquetas es una bioestimulacion que retrasa el proceso de envejecimiento de la piel. Activa la regeneración celular, lo que permite obtener una mejor calidad de piel, más luminosa y lozana, con mejor textura, menos flaccidez y menos arrugas.
+        </p>
+        <p>
+            <h1>HILOS PDO</h1><br>Los hilos de polidioxanona o PDO son unas hebras finas que se introducen bajo la piel generando efectos de soporte para mejorar la flacidez de los tejidos y de estímulo del colageno para redensificar la piel.
+            <br>
+            <br>
+            La revitalización de los tejidos dismiunye el avance del envejecimiento y la flacidez
+        </p>
+        
+        <img id="imagen4" src="./IMG/hilospdo.jpg" alt="">
+    </div>    
+
+        <footer>
+            <div class="pie-de-pagina">
+                <h2>Heftziba</h2>
+                <h3>Contacto: 3023308133</h3>
+                    <nav>
+                        <a href="./index.html" class="active">Inicio</a>
+                        <a href="#">Sobre nosotros</a>
+                        <a href="./Servicios.php">Servicios</a>
+                        <a href="#">Contacto</a>
+                    </nav> 
+                    <br>
+            </div> 
+        </footer>
+    
+        <script src="https://kit.fontawesome.com/34f87c4470.js" crossorigin="anonymous"></script>
+    <script src="./JS/Observer.js"></script>
+</body>
+</html>
